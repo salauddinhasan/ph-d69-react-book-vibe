5abca797c53181720128c39291bfcd217c9bc707
salauddin <salauddin@hasan.com>
2026-04-07 12:10:23 +0600

    feat: implement modern data fetching with Suspense and use hook for Books section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
- import React from 'react';
-import Banner from '../component/Banner';
- 
- const Home = () => {
-    return (
-        <div>
-            <Banner/>
-        </div>
-    );
- };
- 
- export default Home;
+import React, { Suspense } from "react";
+import Banner from "../component/Banner";
+import Books from "../component/Books";
+
+const getData = async () => {
+  const res = await fetch("/data.json");
+   return res.json();
+};
+
+const Home = () => {
+  const getDataPromise = getData();
+  return (
+    <div>
+      <Banner />
+      <Suspense fallback={<p>salauddin...</p>}>
+        <Books getDataPromise={getDataPromise} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Home;
